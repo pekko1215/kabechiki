03bf7710025314ed8c17336399528d6f138ffb4f
pekko1215 <pekko1215@yahoo.co.jp>
2017-10-18 20:29:02 +0900

    dwa

---
+++ token.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'bgr4Y01Xr1qJQGPO8tixyRKlu',
+    consumer_secret: 'pnGG1HXOjxLnHUuyNRGTNbe4V0X485XihSTCfa8sZuLIkUJzH5',
+    access_token_key: '1965603450-aVfpOVNc7bEwz9tbEOfMvM2MiHetU6fuaREe9KZ',
+    access_token_secret: 'BnkBYYtz2nGwRAOZ47n0Yo23u0bq2yDsotjrYFizfAVvh'
+}
--- twitnico.js
+++ twitnico.js
@@ -1,10 +1,5 @@
 var twitter = require('twitter');
-var twit = new twitter({
-    consumer_key: 'bgr4Y01Xr1qJQGPO8tixyRKlu',
-    consumer_secret: 'pnGG1HXOjxLnHUuyNRGTNbe4V0X485XihSTCfa8sZuLIkUJzH5',
-    access_token_key: '1965603450-aVfpOVNc7bEwz9tbEOfMvM2MiHetU6fuaREe9KZ',
-    access_token_secret: 'BnkBYYtz2nGwRAOZ47n0Yo23u0bq2yDsotjrYFizfAVvh'
-});
+var twit = new twitter(require('token'));
 twit.stream('statuses/filter', { 'track': "#slp-kbit" }, function(stream) {
     stream.on('data', function(data) {
         createNicoNico(data.text);
